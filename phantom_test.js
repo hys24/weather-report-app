33d9dcf718d8efb9a21b2124366ec544dccf4b56
hayashitakahiro <Me@hayashitakahiro-no-iMac.local>
2018-12-09 21:34:42 +0900

    a

--- phantom_test.js
+++ phantom_test.js
@@ -2,7 +2,7 @@ var page = require('webpage').create();
 page.open('http://yahoo.co.jp/', function(status) {
   console.log("Status: " + status);
   if(status === "success") {
-    page.render('example.png');
+    page.render('./example.png');
   }
   phantom.exit();
 });
