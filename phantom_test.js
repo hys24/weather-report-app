b382927042975038d2fdec59b6be77effb463562
hayashitakahiro <tkhr.hys.24@gmail.com>
2018-12-09 23:03:19 +0900

    a

---
+++ phantom_test.js
@@ -0,0 +1,22 @@
+var url = 'http://www.metro.tokyo.jp/'
+
+// headlessブラウザを作る
+var page = require('webpage').create();
+
+page.viewportSize = { width: 1200, height: 600 };
+page.clipRect = {
+  top: 0,
+  left: 0,
+  width: 1200,
+  height: 600
+};
+
+// 指定したURLを開く
+page.open(url, function(status) {
+  console.log("Status: " + status);
+  if (status === "success") {
+    page.render('./tmp/image_phantomjs.png');
+  }
+  phantom.exit();
+  //
+});
